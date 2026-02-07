8c85699f8466017d3f86485165d6056b15120ed6
LAB13_USER ON WS21 <L13Y17W21@LabHCI.net>
2026-02-07 16:48:16 +0800

    Initial commit: Setup folder structure for Lab 2

---
+++ backend/src/main/java/Alicaba/example/IT342_G6_Alicaba_Lab1/It342G6AlicabaLab1Application.java
@@ -0,0 +1,13 @@
+package Alicaba.example.IT342_G6_Alicaba_Lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It342G6AlicabaLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It342G6AlicabaLab1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Alicaba/example/IT342_G6_Alicaba_Lab1/It342G6AlicabaLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Alicaba.example.IT342_G6_Alicaba_Lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It342G6AlicabaLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
